446673cf11346880bec2eeb6bbd6999914040a40
Adio Mubarak <adiomubarak1999@gmail.com>
2026-04-30 18:54:37 +0100

    Add job-related views: edit, index, and show templates

---
+++ resources/views/component/layout.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="Author" content="Oxpirant Studio">
+    <meta name="description" content="A job record application built with Laravel.">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
